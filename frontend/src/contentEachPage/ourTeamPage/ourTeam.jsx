39566dca0b06e527c3dcddbc267b928f44fdb6b5
PQuan2003 <147889621+PQuan2003@users.noreply.github.com>
2024-11-07 15:18:06 +0700

    create pages for "services" and "our team"

--- frontend/src/contentEachPage/ourTeamPage/ourTeam.jsx
+++ frontend/src/contentEachPage/ourTeamPage/ourTeam.jsx
@@ -1,11 +1,50 @@
 import React from "react";
-import './ourTeam.css'
+import "./ourTeam.css";
 
-function OurTeam(){
-    return(
-        <div>
-            <h2>Hello </h2>
+function OurTeam() {
+  return (
+    <div class="container">
+      <div className="member-list-header">
+        <h5 className="our-staff">⭐Our Staff</h5>
+        <h2 className="meet-staff">Meet Our Cleaning Staff</h2>
+      </div>
+
+      <section class="cleaning-staff">
+        <div class="staff-members">
+          <div class="staff-member">
+            <img src="http://surl.li/zakygu" alt="" className="member-image" />
+            <div className="member-description">
+              <h3>James Wang</h3>
+              <p>Cleaning Supervisor</p>
+            </div>
+          </div>
+
+          <div class="staff-member">
+            <img src="http://surl.li/wqwwvv" alt="" className="member-image" />
+            <div className="member-description">
+              <h3>Sarah Johnson</h3>
+              <p>Operation Manager</p>
+            </div>
+          </div>
+
+          <div class="staff-member">
+            <img src="http://surl.li/qbgayh" alt="" className="member-image" />
+            <div className="member-description">
+              <h3>David Nguyen</h3>
+              <p>Cleaning Technician</p>
+            </div>
+          </div>
+
+          <div class="staff-member">
+            <img src="http://surl.li/kllirk" alt="" className="member-image" />
+            <div className="member-description">
+              <h3>Samantha Lee</h3>
+              <p>Marketing Coordinator</p>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 export default OurTeam;
