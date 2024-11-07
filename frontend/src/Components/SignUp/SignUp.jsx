5d9a625a7c3f1ea391d6053a3d1fc867792094e7
JetBlack21 <nguyennguyen.210903@gmail.com>
2024-11-07 13:29:00 +0700

    edit footer and signup

--- frontend/src/Components/SignUp/SignUp.jsx
+++ frontend/src/Components/SignUp/SignUp.jsx
@@ -5,8 +5,7 @@ import email_icon from '../../assets/email.png';
 import password_icon from '../../assets/password.png';
 
 const SignUp = () => {
-
-    const[action, setAction] = useState("Sign Up");
+    const [action, setAction] = useState("Sign Up");
 
     return (
         <div className='signup-container'>
@@ -16,32 +15,37 @@ const SignUp = () => {
             </div>
             <div className="signup-inputs">
                 <div className="signup-input">
-                    <img src={user_icon} alt="" />
-                    <input type='text' placeholder='Name'/>
+                    <img src={user_icon} alt="User Icon" />
+                    <input type='text' placeholder={action === "Login" ? 'Username' : 'Name'} />
                 </div>
             </div>
             <div className="signup-inputs">
-                <div className="signup-input">
-                    <img src={email_icon} alt="" />
-                    <input type='email' placeholder='Email address'/>
-                </div>
+                {action === "Login" ? (
+                    <div></div>
+                ) : (
+                    <div className="signup-input">
+                        <img src={email_icon} alt="Email Icon" />
+                        <input type='email' placeholder='Email address' />
+                    </div>
+                )}
             </div>
             <div className="signup-inputs">
-                {action === "Login"?<div></div>:
                 <div className="signup-input">
-                    <img src={password_icon} alt="" />
-                    <input type='password' placeholder='Password'/>
-                </div>}
+                    <img src={password_icon} alt="Password Icon" />
+                    <input type='password' placeholder='Password' />
+                </div>
             </div>
-            {action === "Sign Up" ? <div></div>:
-            <div className="signup-forgot-password">
-                Lost Password? <span>Click Here</span>
-            </div>}
+            {action === "Login" ? (
+                <div className="signup-forgot-password">
+                    Lost Password? <span>Click Here</span>
+                </div>
+            ) : null}
             <div className="signup-submit-container">
-                <div className={action === "Login"?"signup-submit gray":"signup-submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action === "Sign Up"?"signup-submit gray":"signup-submit"} onClick={()=>{setAction("Login")}}>Login</div>
+                <div className={action === "Sign Up" ? "signup-submit gray" : "signup-submit"} onClick={() => setAction("Sign Up")}>Sign Up</div>
+                <div className={action === "Login" ? "signup-submit gray" : "signup-submit"} onClick={() => setAction("Login")}>Login</div>
             </div>
         </div>
     );
 }
+
 export default SignUp;
