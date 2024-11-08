438299e6c5920a6b18fd6078f30b605707e4eb61
Blast2003 <phih2k3@gmail.com>
2024-11-08 11:08:36 +0700

    Add the payment method

--- backend/src/config/variables.js
+++ backend/src/config/variables.js
@@ -6,4 +6,8 @@ export const Variables = {
     PORT: process.env.PORT || 5010,
     MONGO_URL: process.env.MONGO_URL,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+    PAYPAL_SECRET: process.env.PAYPAL_SECRET,
+    PAYPAL_BASE_URL: process.env.PAYPAL_BASE_URL,
+    BASE_URL: process.env.BASE_URL
 }
