ab6bc1861b5b914a1e8bfc51c857b50c64f14937
PQuan2003 <147889621+PQuan2003@users.noreply.github.com>
2024-11-15 22:09:42 +0700

    fix page forwarding for some pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,6 +38,9 @@ import ExaminerFloorPage from './pages/examiner/ExaminerFloor'
 import ExaminerWallPage from './pages/examiner/ExaminerWall'
 import ExaminerFurniturePage from './pages/examiner/ExaminerFurniture'
 
+import UserAgreement from './pages/UserAgreementPage'
+import StaffDetails from './pages/detailStaffPage'
+
 import { useRecoilValue } from 'recoil'
 import customerAtom from './atom/customerAtom'
 import staffAtom from './atom/staffAtom'
@@ -58,49 +61,53 @@ function App() {
       <Routes>
 
         {/* Default Page */}
-        <Route path='/' element={<HomePage/>}/>
-        <Route path="/home" element={<HomePage/>} />
-        <Route path='/aboutUs' element={<AboutPage/>}/>
-        <Route path='/ourTeam' element={<OurTeamPage/>}/>
-        <Route path='/service' element={<ServicePage/>}/>
-        <Route path='/customerApprociate' element={<ApprociatePage/>}/>
-        <Route path='/booking' element={<Booking/>}/>
+        <Route path='/' element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path='/aboutUs' element={<AboutPage />} />
+        <Route path='/ourTeam' element={<OurTeamPage />} />
+        <Route path='/service' element={<ServicePage />} />
+        <Route path='/customerApprociate' element={<ApprociatePage />} />
+        <Route path='/booking' element={<Booking />} />
 
 
         {/* Authentication: Staff + Examiner */}
-        <Route path='/examiner' element={examiner ? <ExaminerPage/> : <SignInExaminer/>}/>
-        <Route path='/staff' element={ staff ? <StaffPage/> : <SignInStaff/>} />
+        <Route path='/examiner' element={examiner ? <ExaminerPage /> : <SignInExaminer />} />
+        <Route path='/staff' element={staff ? <StaffPage /> : <SignInStaff />} />
 
         {/* Service + Task Detail */}
-        <Route path='/carpet' element={<Carpet/>}/>
-        <Route path='/carpetdetail' element={<CarpetNormal/>}/>
-        <Route path='/floor' element={<Floor/>}/>
-        <Route path='/floordetail' element={<FloorNormal/>}/>
-        <Route path='/wall' element={<Wall/>}/>
-        <Route path='/walldetail' element={<WallNormal/>}/>
-        <Route path='/furniture' element={<Furniture/>}/>
-        <Route path='/furnituredetail' element={<FurnitureNormal/>}/>
+        <Route path='/carpet' element={<Carpet />} />
+        <Route path='/carpetdetail' element={<CarpetNormal />} />
+        <Route path='/floor' element={<Floor />} />
+        <Route path='/floordetail' element={<FloorNormal />} />
+        <Route path='/wall' element={<Wall />} />
+        <Route path='/walldetail' element={<WallNormal />} />
+        <Route path='/furniture' element={<Furniture />} />
+        <Route path='/furnituredetail' element={<FurnitureNormal />} />
 
         {/* Customer */}
-        <Route path="/signin" element={customer ? <HomePage/> : <SignIn />} />
-        <Route path="/signup" element={customer ? <HomePage /> : <SignUpPage/>} />
+        <Route path="/signin" element={customer ? <HomePage /> : <SignIn />} />
+        <Route path="/signup" element={customer ? <HomePage /> : <SignUpPage />} />
+        <Route path='/agreement' element={customer ? <UserAgreement /> : <HomePage />} />
+
+        {/*kiểm lại thử cái path vô staff này nha */}
+        <Route path='/staffdetails' element={customer ? <StaffDetails /> : <HomePage />} />
 
 
         {/* staff */}
-        <Route path='/StaffSignin' element={ staff ? <Navigate to={"/staff"}/> : <SignInStaff/>}/>
+        <Route path='/StaffSignin' element={staff ? <Navigate to={"/staff"} /> : <SignInStaff />} />
 
-        <Route path='/staffcarpet' element={staff ? <StaffCarpetPage/> : <SignInStaff/>}/>
-        <Route path='/stafffloor' element={staff ? <StaffFloorPage/> : <SignInStaff/>}/>
-        <Route path='/staffwall' element={staff ? <StaffWallPage/> : <SignInStaff/>}/>
-        <Route path='/stafffurniture' element={staff ? <StaffFurniturePage/> : <SignInStaff/>}/>
+        <Route path='/staffcarpet' element={staff ? <StaffCarpetPage /> : <SignInStaff />} />
+        <Route path='/stafffloor' element={staff ? <StaffFloorPage /> : <SignInStaff />} />
+        <Route path='/staffwall' element={staff ? <StaffWallPage /> : <SignInStaff />} />
+        <Route path='/stafffurniture' element={staff ? <StaffFurniturePage /> : <SignInStaff />} />
 
         {/* examiner */}
-        <Route path='/ExaminerSignin' element={examiner ? <Navigate to={"/examiner"}/> : <SignInExaminer/>}/>
+        <Route path='/ExaminerSignin' element={examiner ? <Navigate to={"/examiner"} /> : <SignInExaminer />} />
 
-        <Route path='/examinercarpet' element={examiner ? <ExaminerCarpetPage/> : <SignInExaminer/>}/>
-        <Route path='/examinerfloor' element={examiner ? <ExaminerFloorPage/> : <SignInExaminer/>}/>
-        <Route path='/examinerwall' element={examiner ? <ExaminerWallPage/> : <SignInExaminer/>}/>
-        <Route path='/examinerfurniture' element={examiner ? <ExaminerFurniturePage/> : <SignInExaminer/>}/>
+        <Route path='/examinercarpet' element={examiner ? <ExaminerCarpetPage /> : <SignInExaminer />} />
+        <Route path='/examinerfloor' element={examiner ? <ExaminerFloorPage /> : <SignInExaminer />} />
+        <Route path='/examinerwall' element={examiner ? <ExaminerWallPage /> : <SignInExaminer />} />
+        <Route path='/examinerfurniture' element={examiner ? <ExaminerFurniturePage /> : <SignInExaminer />} />
       </Routes>
     </div>
   )
